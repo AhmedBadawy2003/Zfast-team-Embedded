7a717e47caf233645e5d91a55a744b75a7e13691
Ahmed Mohamed Badawy <168972698+AhmedBadawy2003@users.noreply.github.com>
2024-12-05 22:45:44 +0200

    Create Bitwise Operations.c

---
+++ Bitwise Operations.c
@@ -0,0 +1,84 @@
+/* Task 1: Bits Manipukation
+   Name: Ahmed Mohamed Badawy */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions Protoytpe */
+
+void setabit(int x,int y);
+void clearabit(int x,int y);
+void toggleabit(int x,int y);
+void checkabit(int x,int y);
+
+/*********/
+
+unsigned int number,position,cases;
+
+int main()
+{
+    number= 59;
+    printf("Current bits state:  00111011\n");
+printf("Please specify the bit you want  :\n");
+scanf("%d",&position);
+
+
+if(position>7 || position < 0)
+{
+    printf("invalid position ");
+
+    return 0;
+}
+printf("Please enter the desired operation:\n 1- Set a bit\n 2- Clear a bit \n 3-Toggle a bit \n 4-Check bit condition\n ");
+scanf("%d",&cases);
+
+
+//10111111
+switch(cases)
+{
+ case(1):
+     setabit(number,position);break;
+ case(2):
+     clearabit(number,position);break;
+ case(3):
+    toggleabit(number,position);break;
+ case(4):
+    checkabit(number,position);break;
+ default:
+    printf("invalid operation!");
+}
+
+return 0;
+}
+
+void setabit(int x,int y)
+{
+    x=x|(1<<y);
+    printf("%d",x);
+}
+
+void clearabit(int x,int y)
+{
+    x=x&~(1<<y);
+printf("%d",x);
+}
+
+void toggleabit(int x,int y)
+{
+    x=x^(1<<y);
+    printf("%d",x);
+}
+
+void checkabit(int x,int y)
+{
+int z;
+
+z=x | (1<<y);
+if(z==x){
+    printf("this bin state is 1");
+
+}
+ else{
+    printf("this bin state is 0");
+ }
+}
